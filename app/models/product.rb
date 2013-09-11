dca4d8d1998e3d7b0862b14d10ff55fcc4aaf86c
kimyee <kimyee.16@gmail.com>
2013-09-11 17:02:51 -0400

    Writing product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price_in_cents
 
+  validates :description, :name, :price_in_cents, :presence => true
+  validates :price_in_cents, :numericality => {:only_integer => true}
 
 end
