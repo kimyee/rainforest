d4780dd4620aa5432fd62b92b4c143181167cd55
kimyee <kimyee.16@gmail.com>
2013-09-10 20:05:02 -0400

    method new

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,36 +3,35 @@ class ProductsController < ApplicationController
   	@products = Product.all
   end
 
-  def show
-  	@product = Product.find(params[:id]) 
-  end
+ #  def show
+ #  	@product = Product.find(params[:id]) 
+ #  end
 
   def new
   	@product = Product.new #gives you a form to create
   end 
 
-	def create
-		@product = Product.new(params[:product])
+	# def create
+	# 	@product = Product.new(params[:product])
 
-		if @product.save
-			redirect_to products_url
-		else
-			render :new
-		end
-	end
+	# 	if @product.save
+	# 		redirect_to products_url
+	# 	else
+	# 		render :new
+	# 	end
+	# end
 	
-	def edit
-		@product = Product.find(params[:id]) #gives you a form to update 	
-	end 
+	# def edit
+	# 	@product = Product.find(params[:id]) #gives you a form to update 	
+	# end 
 	
-	def update
-		@product = Product.find(params[:id])
+	# def update
+	# 	@product = Product.find(params[:id])
 			
-			if @product.update_attribute(params[:product])
-				redirect_to product_path(@product)
-			else
-				render :edit
-			end
-	end
-
+	# 		if @product.update_attribute(params[:product])
+	# 			redirect_to product_path(@product)
+	# 		else
+	# 			render :edit
+	# 		end
+	# end
 end
