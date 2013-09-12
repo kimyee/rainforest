0196bcb0f28131b51e70d8e524109096cb3eb21a
kimyee <kimyee.16@gmail.com>
2013-09-11 21:43:52 -0400

    Creating log in functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  	#don't have to instantiate a user object because only logging in, not creating a new user
+  end
+
+  def create
+  	user = User.find_by_email(params[:email])
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.user_id
+  		redirect_to product_url, :notice => "Logged in!"
+  	else
+  		render "new"
+  	end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to product_url, :notice => "Logged out!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rainforest::Application.routes.draw do
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
   get "users/new"
 
   get "users/create"
 
   resources :products
-  resources :users, :only => [:new, :create]
+  resources :users,   :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
   
   root to: 'products#index' 
 
